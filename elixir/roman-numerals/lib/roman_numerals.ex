f8b7e12d698a0b2ce809c545e127742da6d56f85
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-03-10 17:23:47 -0800

    working a bit more on the roman numerals exericse

--- elixir/roman-numerals/lib/roman_numerals.ex
+++ elixir/roman-numerals/lib/roman_numerals.ex
@@ -5,7 +5,11 @@ defmodule RomanNumerals do
   @romans %{1 => "I"}
 
   @spec numeral(pos_integer) :: String.t()
-  def numeral(number) do
-    @romans[number]
+  def numeral(1), do: @romans[1]
+  def numeral(num, acc) when num < 4 do
+    "acc<>I"
+
   end
+
+
 end

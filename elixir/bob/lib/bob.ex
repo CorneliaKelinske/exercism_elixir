3bad9fc6019a35459978c5554675f61a329c0914
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-03-22 14:37:29 -0700

    getting more tests to pass

--- elixir/bob/lib/bob.ex
+++ elixir/bob/lib/bob.ex
@@ -1,7 +1,14 @@
 defmodule Bob do
   def hey(input) do
     cond do
+
+      String.trim(input) === "" -> "Fine. Be that way!"
+      String.ends_with?(input, "?") && String.upcase(input) == input -> "Calm down, I know what I'm doing!"
+
+      String.upcase(input) == input -> "Whoa, chill out!"
+      String.ends_with?(input, "?") -> "Sure."
       String.length(input) > 0 -> "Whatever."
+
     end
   end
 end

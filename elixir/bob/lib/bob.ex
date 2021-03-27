c73f0bfdfe122ec71381fa8528984dda2c3f6c06
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-03-27 12:07:42 -0700

    spruced up and improved the bob code

--- elixir/bob/lib/bob.ex
+++ elixir/bob/lib/bob.ex
@@ -1,16 +1,19 @@
 defmodule Bob do
   def hey(input) do
+    trimmed = String.trim(input)
+
     cond do
-
-      String.trim(input) === "" -> "Fine. Be that way!"
-      String.match?(String.trim(input), ~r/[[:alpha:]]/u) == false && String.ends_with?(input, "?") -> "Sure."
-      String.match?(input, ~r/[[:alpha:]]/u) == false -> "Whatever."
-      String.ends_with?(input, "?") && String.upcase(input) == input -> "Calm down, I know what I'm doing!"
-      String.upcase(input) == input -> "Whoa, chill out!"
-      String.ends_with?(String.trim(input), "?") -> "Sure."
-      String.length(input) > 0 -> "Whatever."
-      true -> "Did you say something?"
-
+      empty_string?(input) -> "Fine. Be that way!"
+      yelling?(trimmed) && question?(trimmed) -> "Calm down, I know what I'm doing!"
+      question?(trimmed) -> "Sure."
+      yelling?(input) -> "Whoa, chill out!"
+      true -> "Whatever."
     end
   end
+
+  defp empty_string?(input), do: String.trim(input) === ""
+
+  defp yelling?(input), do: String.downcase(input) != input and String.upcase(input) == input
+
+  defp question?(input), do: String.ends_with?(input, "?")
 end

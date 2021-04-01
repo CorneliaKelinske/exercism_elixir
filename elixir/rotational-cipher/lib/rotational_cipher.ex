30709806e5439d09ea70818594b566f6a560bd7f
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-04-01 14:14:58 -0700

    finished the cipher exercise

--- elixir/rotational-cipher/lib/rotational_cipher.ex
+++ elixir/rotational-cipher/lib/rotational_cipher.ex
@@ -7,40 +7,31 @@ defmodule RotationalCipher do
   "Nggnpx ng qnja"
   """
 
-
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
     text
     |> String.to_charlist()
     |> shift(shift)
     |> List.to_string()
-    |> IO.inspect
   end
-
 
   defp shift(charlist, shift) do
     Enum.map(charlist, fn char -> encode(char, shift) end)
   end
 
   defp encode(char, shift) when char >= 97 and char <= 122 do
-    case rem(char-96+shift, 26) + 96 do
+    case rem(char(-96 + shift, 26)) + 96 do
       96 -> 'z'
       new_char -> new_char
     end
   end
 
+  defp encode(char, shift) when char >= 65 and char <= 90 do
+    case rem(char - 64 + shift, 26) + 64 do
+      90 -> 'z'
+      new_char -> new_char
+    end
+  end
 
-  # defp is_lower?(char) do
-  #   char >= 97 && char <= 122
-  # end
-
-  # defp is_upper?(char) do
-  #   char >= 65 && char <= 90
-  # end
-
-
-
-
-
-
+  defp encode(char, shift), do: char
 end

b2bb70e3ca937ffaeba1f842bf8c610b0506fe59
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-04-15 07:17:58 -0700

    starting on the of_rna function

--- elixir/protein-translation/lib/protein_translation.ex
+++ elixir/protein-translation/lib/protein_translation.ex
@@ -25,7 +25,20 @@ defmodule ProteinTranslation do
   }
   @spec of_rna(String.t()) :: {atom, list(String.t())}
   def of_rna(rna) do
+    rna
+    |> String.graphemes
+    |> Enum.chunk_every(3)
+    |> Enum.map(fn x -> Enum.join(x) end)
+    |> Enum.reduce([], fn x, acc -> [get_protein(x)| acc] end)
+    |> Enum.reverse
+    |> create_tuple
+
+
   end
+
+  defp get_protein(codon), do: @codons[codon]
+
+  defp create_tuple(protein_list), do: {:ok, protein_list}
 
   @doc """
   Given a codon, return the corresponding protein

03b1ebf7f26dbee9ce3e6edd6fca456ef284f012
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-05-07 06:49:33 -0700

    refactored code for resistor color exercise

--- elixir/resistor-color/lib/resistor_color.ex
+++ elixir/resistor-color/lib/resistor_color.ex
@@ -1,37 +1,30 @@
 defmodule ResistorColor do
   @moduledoc false
-  @color_codes %{
-    "black" => 0,
-    "brown" => 1,
-    "red" => 2,
-    "orange" => 3,
-    "yellow" => 4,
-    "green" => 5,
-    "blue" => 6,
-    "violet" => 7,
-    "grey" => 8,
-    "white" => 9
-  }
+  @color_codes [
+    {"black", 0},
+    {"brown", 1},
+    {"red",  2},
+    {"orange", 3},
+    {"yellow", 4},
+    {"green", 5},
+    {"blue", 6},
+    {"violet", 7},
+    {"grey", 8},
+    {"white", 9}
+  ]
 
 
   @spec colors() :: list(String.t())
   def colors do
-    colors = [
-      "black",
-      "brown",
-      "red",
-      "orange",
-      "yellow",
-      "green",
-      "blue",
-      "violet",
-      "grey",
-      "white"
-    ]
+    for {color, code} <- @color_codes do
+      color
+    end
   end
 
   @spec code(String.t()) :: integer()
   def code(color) do
-    @color_codes[color]
+    @color_codes
+    |> Enum.find(& elem(&1, 0) == color)
+    |> elem(1)
   end
 end

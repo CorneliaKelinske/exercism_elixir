a597e3f9bdf91670416705f05bf443fc7b0eaf56
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-05-13 12:35:30 -0700

    making some progress on the acronym exercise

--- elixir/acronym/lib/acronym.ex
+++ elixir/acronym/lib/acronym.ex
@@ -6,17 +6,17 @@ defmodule Acronym do
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
     string
-    |> String.split("-")
+    |> String.split()
     |> split_up_hyphenated()
     |> Enum.reduce("", & &2 <> String.first(&1))
     |> String.upcase()
   end
 
-  defp split_up_hyphenated(string) do
+  def split_up_hyphenated(string) do
     Enum.map(string, fn x ->
-      case String.contains?(x, "-") do
-        true -> String.split(x, "-")
-        false -> x
+      cond do
+        String.contains?(x, "-") == true -> String.split(x, "-")
+        String.contains?(x, "-") == false -> x
       end
     end)
 

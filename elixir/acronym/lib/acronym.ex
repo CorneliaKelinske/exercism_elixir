63203f8d13deca2b1e4938d6a44c8e3a4461d899
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-05-15 17:35:25 -0700

    now everything is failing... hmmmm

--- elixir/acronym/lib/acronym.ex
+++ elixir/acronym/lib/acronym.ex
@@ -5,23 +5,53 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
+    do_abbreviate(string)
+  end
+
+  defp do_abbreviate(string) do
+    if is_exception?(string) do
+      do_exception(string)
+    else
+      do_normal(string)
+    end
+  end
+
+  defp is_exception?(string) do
+    Regex.match?(~r/^[A-Z]|[A-Z](?:[a-z])/, string)
+  end
+
+  defp do_exception(string) do
     Regex.scan(~r/^[A-Z]|[A-Z](?:[a-z])/, string)
-    |> IO.inspect()
+    |> Enum.map(fn(x) -> List.to_string(x) end)
     |> Enum.map(fn(x) -> List.to_string(x) end)
     |> Enum.map(fn(x) -> String.first(x) end)
-    |> IO.inspect()
     |> Enum.join
     |> String.upcase
+  end
 
+  defp do_normal(string) do
+    string
+    |> String.replace(["-", "_"], " ")
+    |> String.split()
+    |> Enum.reduce("", & &2 <> String.first(&1))
+    |> String.upcase()
   end
 
 
 end
 
-#Regex.scan(~r/^[A-Z]|[A-Z](?:[a-z])/,"Portable Networks Graphic")
+
 
 # string
 #     |> String.replace(["-", "_"], " ")
 #     |> String.split()
 #     |> Enum.reduce("", & &2 <> String.first(&1))
 #     |> String.upcase()
+
+# Regex.scan(~r/^[A-Z]|[A-Z](?:[a-z])/, string)
+#     |> IO.inspect()
+#     |> Enum.map(fn(x) -> List.to_string(x) end)
+#     |> Enum.map(fn(x) -> String.first(x) end)
+#     |> IO.inspect()
+#     |> Enum.join
+#     |> String.upcase

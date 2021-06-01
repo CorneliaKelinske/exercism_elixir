7357f308d5cefb43c7058cfb0f0c7a85740ef42e
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-06-01 07:40:10 -0700

    deleted the IO.inspects and changed a few more variable names

--- elixir/markdown/lib/markdown.ex
+++ elixir/markdown/lib/markdown.ex
@@ -16,11 +16,8 @@ defmodule Markdown do
   def parse(m) do
     m
     |> String.split("\n")
-    |> IO.inspect(label: "output of String.split")
     |> Enum.map(& process(&1))
-    |> IO.inspect(label: "Enum.map process output")
     |> Enum.join()
-    |> IO.inspect(label: "output of Enum.join")
     |> patch()
 
   end
@@ -39,54 +36,55 @@ defmodule Markdown do
   defp parse_header_md_level(line) do
     [h | t] = String.split(line)
     {to_string(String.length(h)), Enum.join(t, " ")}
-    |> IO.inspect(label: "output of parse_header_md_level")
     # {"number", "text minus the header markup"}
   end
 
   #changed the argument to "line" for the same reason as in the parse_header_md_function above. also changed "t" to "text"
   defp parse_list_md_level(line) do
-    IO.inspect(line, label: "Input to parse_list_md_level")
     text = String.split(String.trim_leading(line, "* "))
-    IO.inspect(text, label: "This is text")
     "<li>" <> join_words_with_tags(text) <> "</li>"
-    |> IO.inspect(label: "parse_list_md_level output")
   end
 
   defp enclose_with_header_tag({hl, htl}) do
     "<h" <> hl <> ">" <> htl <> "</h" <> hl <> ">"
   end
 
-  defp enclose_with_paragraph_tag(line) do
-    "<p>#{join_words_with_tags(line)}</p>"
+  #I changed the argument to split_line to reflect that it is the result of String.split(line) in the process(line) function
+  defp enclose_with_paragraph_tag(split_line) do
+    "<p>#{join_words_with_tags(split_line)}</p>"
   end
 
-  defp join_words_with_tags(t) do
-    Enum.join(Enum.map(t, fn w -> replace_md_with_tag(w) end), " ")
+  #I changed the argument to unsplit_text
+  defp join_words_with_tags(unsplit_text) do
+    Enum.join(Enum.map(unsplit_text, fn word -> replace_md_with_tag(word) end), " ")
+
   end
 
-  defp replace_md_with_tag(w) do
-    replace_suffix_md(replace_prefix_md(w))
+  #I changed the argument name from "w" to "word" everywhere
+  defp replace_md_with_tag(word) do
+    replace_suffix_md(replace_prefix_md(word))
   end
 
-  defp replace_prefix_md(w) do
+  defp replace_prefix_md(word) do
     cond do
-      w =~ ~r/^#{"__"}{1}/ -> String.replace(w, ~r/^#{"__"}{1}/, "<strong>", global: false)
-      w =~ ~r/^[#{"_"}{1}][^#{"_"}+]/ -> String.replace(w, ~r/_/, "<em>", global: false)
-      true -> w
+      word =~ ~r/^#{"__"}{1}/ -> String.replace(word, ~r/^#{"__"}{1}/, "<strong>", global: false)
+      word =~ ~r/^[#{"_"}{1}][^#{"_"}+]/ -> String.replace(word, ~r/_/, "<em>", global: false)
+      true -> word
     end
   end
 
-  defp replace_suffix_md(w) do
+  defp replace_suffix_md(word) do
     cond do
-      w =~ ~r/#{"__"}{1}$/ -> String.replace(w, ~r/#{"__"}{1}$/, "</strong>")
-      w =~ ~r/[^#{"_"}{1}]/ -> String.replace(w, ~r/_/, "</em>")
-      true -> w
+      word =~ ~r/#{"__"}{1}$/ -> String.replace(word, ~r/#{"__"}{1}$/, "</strong>")
+      word =~ ~r/[^#{"_"}{1}]/ -> String.replace(word, ~r/_/, "</em>")
+      true -> word
     end
   end
 
-  defp patch(l) do
+  #Once again changed "l" to "line" for more clarity
+  defp patch(line) do
     String.replace_suffix(
-      String.replace(l, "<li>", "<ul>" <> "<li>", global: false),
+      String.replace(line, "<li>", "<ul>" <> "<li>", global: false),
       "</li>",
       "</li>" <> "</ul>"
     )

9fbda11027c9dba6763a2ba4b4c78c12486b4c6a
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-06-12 11:38:24 -0700

    made some more changes to the account.exs file; getting ready to transport the code into the actual exercise file

--- elixir/bank-account/lib/account.exs
+++ elixir/bank-account/lib/account.exs
@@ -3,11 +3,15 @@ defmodule Account do
 
   #Client
   def open_bank do
-    GenServer.start_link(__MODULE__, %{balance: 0, status: :open})
+    with {:ok, pid} <- GenServer.start_link(__MODULE__, %{balance: 0, status: :open}) do
+      pid
+    else
+      error -> raise "Account was not opened. #{inspect(error)}"
+    end
   end
 
   def update(pid, amount) do
-    GenServer.cast(pid, amount)
+    GenServer.cast(pid, {:deposit, amount)}
   end
 
   def view(pid) do
@@ -23,26 +27,24 @@ defmodule Account do
     {:ok, account}
   end
 
-  def handle_cast(amount, account) do
-    updated_account = Map.put(account, :balance, account.balance + amount)
-    {:noreply, updated_account}
-  end
-
   def handle_cast(:account_closed, account) do
     updated_account = Map.put(account, :status, :account_closed)
     {:noreply, updated_account}
   end
 
-  def handle_cast(_, account = %{status: :account_closed}) do
-    {:reply, {:error, :account_closed}, account}
+  def handle_cast({amount}, account) when is_integer(amount) do
+    updated_account = Map.put(account, :balance, account.balance + amount)
+    {:noreply, updated_account}
   end
+
+  def handle_call(_, from, account = %{status: :account_closed}) do
+    {:reply, {:error, :account_closed}, account}  end
+
 
   def handle_call(:view, from, account) do
     {:reply, account, account}
   end
 
-  def handle_call(_, from, account = %{status: :account_closed}) do
-    {:reply, {:error, :account_closed}, account}
-  end
+
 
 end

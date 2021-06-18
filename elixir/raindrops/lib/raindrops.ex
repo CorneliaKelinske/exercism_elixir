2e80e89ec2f81e09bbf5d7994abefd67c7882242
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-06-18 06:35:23 -0700

    solution using defguards

--- elixir/raindrops/lib/raindrops.ex
+++ elixir/raindrops/lib/raindrops.ex
@@ -9,10 +9,31 @@ defmodule Raindrops do
     just pass the number's digits straight through.
   """
 
-
+defguard is_pling(number) when rem(number, 3) == 0
+defguard is_plang(number) when rem(number, 5) == 0
+defguard is_plong(number) when rem(number, 7) == 0
 
   @spec convert(pos_integer) :: String.t()
-  def convert(number) when is_pling(number) and is
-
+  def convert(number) when is_pling(number) and is_plang(number) and is_plong(number) do
+    "PlingPlangPlong"
   end
+
+  def convert(number) when is_pling(number) and is_plang(number) do
+    "PlingPlang"
+  end
+
+  def convert(number) when is_pling(number) and is_plong(number) do
+    "PlingPlong"
+  end
+
+  def convert(number) when is_plang(number) and is_plong(number) do
+    "PlangPlong"
+  end
+
+  def convert(number) when is_pling(number), do: "Pling"
+  def convert(number) when is_plang(number), do: "Plang"
+  def convert(number) when is_plong(number), do: "Plong"
+  def convert(number), do: to_string(number)
+
+
 end

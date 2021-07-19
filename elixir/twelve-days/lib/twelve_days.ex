696fda382f7cdc6467a9598de77032375b6cc4a7
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-07-19 14:24:06 -0700

    made one last change, using Enum.map instead of for

--- elixir/twelve-days/lib/twelve_days.ex
+++ elixir/twelve-days/lib/twelve_days.ex
@@ -59,9 +59,8 @@ defmodule TwelveDays do
   """
   @spec verses(starting_verse :: integer, ending_verse :: integer) :: String.t()
   def verses(starting_verse, ending_verse) do
-    for verse <- starting_verse..ending_verse do
-      "#{verse(verse)}\n"
-    end
+    starting_verse..ending_verse
+    |> Enum.map(& "#{verse(&1)}\n")
     |> List.to_string()
     |> String.trim_trailing("\n")
   end

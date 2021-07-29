6700d9f8ffb21d371722191f7d38068933e46d81
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-07-29 11:32:24 -0700

    refactored using &

--- elixir/sum-of-multiples/lib/sum_of_multiples.ex
+++ elixir/sum-of-multiples/lib/sum_of_multiples.ex
@@ -2,22 +2,11 @@ defmodule SumOfMultiples do
   @doc """
   Adds up all numbers from 1 to a given end number that are multiples of the factors provided.
   """
-  @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
-  # def to(limit, factors) do
-  #   Enum.reduce(0..limit, 0, fn x, acc ->
-  #     Enum.map(factors, fn factor ->
-  #       cond do
-  #         rem(x, factor) == 0 -> x
-  #         true -> 0
-  #       end
-  #     end)
-  #   end)
-  #   |> Enum.sum
-  # end
+
 
   def to(limit, factors) do
-    Enum.map(0..limit-1, fn(x) -> get_factor_sum(x, factors) end)
-    |> Enum.sum()
+    Enum.reduce(0..limit-1, 0, & &2 + get_factor_sum(&1, factors))
+
 
   end
 

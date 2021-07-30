408694f6e1618edc565daf00cd4b8f47ba2721c7
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-07-30 06:52:14 -0700

    had to re-do the pipeline clean-up

--- elixir/sum-of-multiples/lib/sum_of_multiples.ex
+++ elixir/sum-of-multiples/lib/sum_of_multiples.ex
@@ -3,15 +3,13 @@ defmodule SumOfMultiples do
   Adds up all numbers from 1 to a given end number that are multiples of the factors provided.
   """
 
-
   def to(limit, factors) do
-    Enum.reduce(0..limit-1, 0, & &2 + get_factor_sum(&1, factors))
-
-
+    Enum.reduce(0..(limit - 1), 0, &(&2 + get_factor_sum(&1, factors)))
   end
 
   def get_factor_sum(number, factors) do
-    Enum.map(factors, fn(x) ->
+    factors
+    |> Enum.map(fn x ->
       cond do
         rem(number, x) == 0 -> number
         true -> 0
@@ -19,7 +17,5 @@ defmodule SumOfMultiples do
     end)
     |> Enum.uniq()
     |> Enum.sum()
-
-
   end
 end

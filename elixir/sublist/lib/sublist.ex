4811dd59f6795dd1c7264443fa9b250a844f29bc
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-08-18 14:47:29 -0700

    getting started on the sublist exercise; easy tests are passing

---
+++ elixir/sublist/lib/sublist.ex
@@ -0,0 +1,12 @@
+defmodule Sublist do
+  @doc """
+  Returns whether the first list is a sublist or a superlist of the second list
+  and if not whether it is equal or unequal to the second list.
+  """
+  def compare([], []), do: :equal
+
+  def compare([], list), do: :sublist
+
+  def compare(list, []), do: :superlist
+
+end

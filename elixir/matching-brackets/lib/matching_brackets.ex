cba2a88c684d8c2324b304e886c1bf838f371293
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-09-01 17:08:38 -0700

    started from scratch with my matching brackets exercise and succeeded!

--- elixir/matching-brackets/lib/matching_brackets.ex
+++ elixir/matching-brackets/lib/matching_brackets.ex
@@ -2,13 +2,21 @@ defmodule MatchingBrackets do
   @doc """
   Checks that all the brackets and braces in the string are matched correctly, and nested correctly
   """
-  @closing_brackets %{"(" => ")", "{" => "}", "[" => "]"}
+  @matching_brackets ["()", "[]", "{}"]
+
+  def check_brackets("") do
+    true
+  end
 
   @spec check_brackets(String.t()) :: boolean
   def check_brackets(str) do
     str = Regex.replace(~r/[^(){}\[\]]/, str, "")
-    str = String.codepoints(str)
-    Enum.map(str, fn x -> List.delete(str, @closing_brackets[x]) end)
-  end
+    new_str = String.replace(str, @matching_brackets, "")
 
+    if new_str != str do
+      check_brackets(new_str)
+    else
+      false
+    end
+  end
 end
